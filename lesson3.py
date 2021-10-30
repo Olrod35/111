4ccac61c79bf58f3b8b293fd25e2b689d5cf7c58
Olrod35 <ovrodkina@gmail.com>
2021-10-30 12:03:12 +0300

    change to lesson3

--- lesson3.py
+++ lesson3.py
@@ -1,3 +1,4 @@
+'''
 a = 2
 b = 6
 c = None
@@ -5,6 +6,27 @@ d = 'string'
 e = 6
 
 if c is None or e-b:
-    print('False')
+    print('ok')
 else:
     print('Else')
+'''
+'''
+if isinstance(1.0, (int, float)):
+    print('ok')
+else:
+    print('false')
+'''
+a = "dddd"
+b = 'gggg\n hhh'\
+    'mmmm'
+c = '''
+nnnn
+mmmm
+'''
+d = f"text {c}"
+print(d)
+
+print(a)
+print(b)
+print(c)
+
