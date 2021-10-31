62e78be0afe9a1d4f23e34244c868312b99db4d6
Olrod35 <ovrodkina@gmail.com>
2021-10-31 15:12:17 +0200

    change to home3

---
+++ home3.py
@@ -0,0 +1,22 @@
+a_initial = 123
+b_digit = 0
+d_digit = 0
+e_digit = 0
+f_reverse = 0
+
+b_digit = a_initial // 100
+_c_ = 0
+_c_ = a_initial % 100
+d_digit = _c_ // 10
+e_digit = a_initial % 10
+f_reverse = e_digit*100 + d_digit*10 + b_digit
+print(a_initial)
+print(f_reverse)
+
+
+
+
+
+
+
+
