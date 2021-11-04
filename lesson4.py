265b97f7699ffc61dcd9c42eb15b232de2b27741
Olrod35 <ovrodkina@gmail.com>
2021-11-04 21:35:56 +0200

    change to home3

---
+++ lesson4.py
@@ -0,0 +1,18 @@
+a = [1, 2, 3]
+a.append(99)
+print(a)
+a.reverse()
+print(a)
+'''for i in range(50):
+       print(i)
+'''
+'''b = 4
+for _ in range(3):
+    b = b + 2
+    print(b)'''
+fig_height = input()
+for i in range(fig_height):
+    i = '*************'
+    print(i)
+
+
