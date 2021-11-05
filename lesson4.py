42b091bbb2c884706eb29535408d149efb3dfc48
Olrod35 <ovrodkina@gmail.com>
2021-11-05 22:44:03 +0200

    change to home4

--- lesson4.py
+++ lesson4.py
@@ -1,34 +1,27 @@
-a = [1, 2, 3]
-a.append(99)
-print(a)
-a.reverse()
-print(a)
-'''for i in range(50):
-       print(i)
-'''
-'''b = 4
-for _ in range(3):
-    b = b + 2
-    print(b)'''
 
-i = 1
+'''i = 1
 d = '*'
 s = 0
-p = '         '
-m = 7
-#a = 0
+p = '                              '
+n = input('Введите высоту треугольника: ')
+n = int(n)
+m = n
 
-while s < 7:
-    a = print(p[0:m] + (d * i))
-    #a.reverse()
+    while s < n:
+    a = print(p[0:m] + (d * i) + d * (i-1))
     i += 1
     m -= 1
     s += 1
+    '''
 
+d = '*'
+n = int(input('Введите высоту треугольника: '))
+d = '*'
+p = n * " "
+m = n
+for i in range(n):
+    a = print(p[0:m] + d * (i + 1) + d * i)
+    m -= 1
 
-'''fig_height = input()
-for i in range(fig_height):
-    i = '*************'
-    print(i)'''
 
 
