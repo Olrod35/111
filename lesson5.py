84dd6efbd4e49e47d0477ca45c5fe0f804ca7733
Olrod35 <ovrodkina@gmail.com>
2021-11-08 22:16:48 +0200

    change to home4

--- lesson5.py
+++ lesson5.py
@@ -1,3 +1,25 @@
-import random
-random.randint(1, 10)
+'''import random
+a = [random.randint(1, 10) for _ in range(10)]
+b = [random.randint(1, 10) for _ in range(10)]
+print(a)
+print(b)
+i=0
+j=0
+c = []
+for i in a:
+    for j in b:
+    if b[2] == 0:
+ b[2]
+        print(c)
+    i +=1
+    j +=1
+    '''
+# Дз7 Списки
+i = 1
+a = [random.randint(1, 10) for _ in range(15)]
+print(a)
+for i in a:
+if a[i] > a[i+1] and a[i] > a[i-1]:
+    b += 1
+    return b
 
