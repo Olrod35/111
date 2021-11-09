fc4e3613f9da31db67afb735e30461016c7bd269
Olrod35 <ovrodkina@gmail.com>
2021-11-09 22:46:23 +0200

    change to home6

--- lesson5.py
+++ lesson5.py
@@ -1,16 +1,14 @@
-# Дз7 Списки
+# Дз6 Списки
+
 import random
 a = [random.randint(1, 10) for _ in range(15)]
-print(a)
-b = len([i for i in a if (a[i+1] > a[i] and a[i+1] > a[i+2])])
-print(b)
-
-"""b = []
-a = [random.randint(1, 10) for _ in range(15)]
-print(a)
-if all(i for i in a if (i+1) > i):
-    b = [i for i in a if (i+1) > 8]
-    print(b)
-    print(len(b))"""
-
-
+print(a) #для проверки начального списка
+b = [random.randint(1, 10) for _ in range(15)]
+print(b) #для проверки начального списка
+c = list(set(a) & set(b)) #основной код
+print(c)  #основной код
+print(len(c))  #основной код
+unique_numbers_a = list(set(a))
+print(unique_numbers_a) #для проверки списка с уникальными числами
+unique_numbers_b = list(set(b))
+print(unique_numbers_b) #для проверки списка с уникальными числами
