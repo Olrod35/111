5b457251ceb417eda186e7e7ff435a6a7b80bc42
Olrod35 <ovrodkina@gmail.com>
2021-11-16 22:57:10 +0200

    Lesson7

--- Lesson7Home.py
+++ Lesson7Home.py
@@ -31,3 +31,25 @@ def arithmetic(a, b, z):
 
 z = input("Введите операцию '+','-','*' или '/': ")
 print(arithmetic(5, 6, z))
+
+#ДЗ14
+"""Дано положительное целое число. Вам необходимо подсчитать произведение всех цифр в этом числе, за исключением нулей.
+Для примера: Дано число 123405. Результат будет: 1*2*3*4*5=120 (не забудьте исключить нули).
+Пример:
+checkio(123405) == 120
+checkio(999) == 729
+checkio(1000) == 1
+checkio(1111) == 1"""
+
+def checkio(k):
+    stroka = str(k)
+    spisok = []
+    for i in stroka:
+        spisok.append(int(i))
+    summa = sum(spisok)
+    return summa
+
+k = input("Введите целое число: ")
+print(checkio(k))
+
+
