790865dc07a20e35bc0c26f884a78552c4029acb
Olrod35 <ovrodkina@gmail.com>
2021-11-21 12:20:59 +0200

    Lesson9

---
+++ Lesson8Home.py
@@ -0,0 +1,18 @@
+#ДЗ 15. Декоратор
+def check_password() -> bool;
+
+
+def authenticate() -> bool;
+
+
+def login(username, password) -> bool;
+for i in range(3):
+    if check = "True"
+    yield print("Вы в системе!")
+    if check = "False":
+        yield print("Неправильное имя или пароль")
+
+"Попытки истекли"
+"У Вас осталось N попыток"
+"Вы в системе!"
+"Неправильное имя или пароль"
