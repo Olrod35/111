243f7432a06371c5c284a096c7f3cb5acee298cb
Olrod35 <ovrodkina@gmail.com>
2021-11-28 22:27:05 +0200

    Lesson11

---
+++ Lesson9Home16.py
@@ -0,0 +1,52 @@
+#ДЗ16
+"""В программу написанную в прошлом ДЗ добавить новый функционал (не изменяя старого)
+username and password получать из командной строки как не обязательные аргументы.
+Если оба аргумента переданы и имя и пароль прошли проверку то программа завершает работу не меняя прошлого поведения.
+Если не правильное имя или пароль даем 3 попытки как и раньше.
+Если передать только имя, то спрашивать только пароль, и наоборот, спрашивать пароль если передать только имя.
+Основная идея в том что-бы расширить функционал прошлой программы, а не переписывать!"""
+
+from functools import wraps
+
+USERS = {
+    'Olha': '111111',
+    'Talia': '222222'
+}
+
+def decorator_for_login(func):
+    def wrapper(username, password):
+        if not check_password(username, password):
+            return False
+        if not auth():
+            return False
+        return func(username, password)
+    return wrapper
+
+def auth():
+    return True
+
+def check_password(username, password):
+    if USERS.get(username) == password:
+        return True
+    #return USERS.get(username) == password
+
+@decorator_for_login
+def login(username, password):
+    print("Вы в системе!")
+    return True
+
+def main():
+    i = 3
+    while i > 0:
+        print("У Вас осталось", i, "попыток")
+        username = input("Введите Ваш логин:")
+        password = input("Введите Ваш пароль:")
+        if login(username, password):
+            break
+        print("Неправильное имя или пароль")
+        i -= 1
+        if i == 0:
+            print("Попытки истекли")
+
+if __name__ == "__main__":
+    main()
